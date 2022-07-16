8a0002c8147c56ce90bf62c0f6b9c928e37de371
Akshat Kumar <akshatkumar4433@gmail.com>
2022-07-16 02:04:53 -0400

    minor fixes

--- server.js
+++ server.js
@@ -10,10 +10,31 @@ app.get('/',(request, response) => {
     response.sendFile('index.html')
 });
 
-
+/*
 app.get('/resume', async (request, response) => {
     response.download(path.join(__dirname, "resume/resume.pdf"));
 });
+*/
+
+app.get('/resume' , (request, response,next) => {
+    const options = {
+        root: path.join(__dirname, 'resume'),
+        dotfiles: 'deny',
+        headers: {
+          'x-timestamp': Date.now(),
+          'x-sent': true
+        }
+      };
+      
+      var fileName = 'resume.pdf';
+      response.sendFile(fileName, options, function (err) {
+        if (err) {
+          console.log('err', err);
+      
+          next(err);
+        }
+      });
+})
 
 
 app.listen(port, () => {
