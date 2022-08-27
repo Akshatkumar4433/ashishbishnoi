e742f5eca44f67f5f36de4f95fd93f646b78ecff
Akshat Kumar <akshatkumar4433@gmail.com>
2022-08-26 22:50:41 -0400

    added express

--- src/components/header1/Header1.js
+++ src/components/header1/Header1.js
@@ -5,6 +5,21 @@ export default class Header1 extends React.Component {
         return(
             <div className = 'header1'>
                 <h1><span className = 'header-text'>Enviromental Technologist</span></h1>
+                <ul class = 'summary-list'>
+                    <li>Solid understanding of Environmental Legislation and Regulations.</li>
+                    <li>Strong awareness of health and safety issues, ability in identifying and reporting
+potential and existing site hazards.</li>
+                     <li>Outstanding interpersonal and relationship building skills; skilled at building and
+maintaining industry networks.</li>
+                    <li>Excellent Knowledge of the occupational health & safety act, and the regulations that apply to the
+work including confined space entry, traffic control, WHMIS, first aid, etc.</li>
+                <li>Detail- oriented and organized; able to work well under pressure and meet
+deadlines quickly.</li>
+                <li>Strong attention to details with excellent analytical and judgement capabilities.</li>
+                <li>Proficient report writing and IT skills, including the ability to use MS Word and MS
+excel.</li>
+                </ul>
+
             </div>
         )
     }
--- src/components/navigation/NavigationItems.js
+++ src/components/navigation/NavigationItems.js
@@ -10,7 +10,7 @@ export const navigationItems = [
         url:'/',
     },
     {
-        title: 'Linkden',
+        title: 'linkedln',
         logo:'',
         url:'/',
     },
